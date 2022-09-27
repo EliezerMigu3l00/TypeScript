a08972ae241a0ba4e1b5c1c6e22c89168444cca5
Eliezer Miguel <eliezermiguel97@outlook.com>
2022-09-26 22:39:37 -0300

    Atl tus

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,14 +1,18 @@
+import { Negociacoes } from "../models/negociacoes.js";
 import { Negociacao } from "../models/nogociacao.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update();
     }
 
     adiciona(): void{
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -11,4 +11,3 @@ export class Negociacoes {
         return this.negociacoes;
     }
 }
- 
