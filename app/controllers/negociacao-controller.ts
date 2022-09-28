3e9c2731f79c54ccd4672180855aae3984ac32fb
Eliezer Miguel <eliezermiguel9@outlook.com>
2022-09-28 12:33:09 -0300

    Criacao de view e reiclagem de codigo

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,23 +1,29 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { Negociacao } from "../models/nogociacao.js";
+import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');  
 
     constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
-        this.negociacoesView.update();
+        this.negociacoesView.update(this.negociacoes);
     }
 
     adiciona(): void{
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
+        this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
@@ -35,3 +41,4 @@ export class NegociacaoController {
         this.inputData.focus();
     }
 }
+
--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,10 +1,9 @@
-export class NegociacoesView {
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
-    private elemento: HTMLElement;
-    constructor(seletor: string){
-        this.elemento = document.querySelector(seletor);
-    }
-    template(): string {
+export class NegociacoesView extends View<Negociacoes> {
+
+    template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -14,13 +13,24 @@ export class NegociacoesView {
                     <th>VALOR</th>
                 </tr>
             </thead>
+            <tbody>
+                ${model.lista().map(negociacao => {
+                    return `
+                        <tr>
+                            <td>
+                                ${new Intl.DateTimeFormat().format(negociacao.data)}
+                            </td>
+                            <td>
+                                ${negociacao.quantidade}
+                            </td>
+                            <td>
+                                ${negociacao.valor}
+                            </td>
+                        </tr>
+                    `;
+                }).join('')}
+            </tbody>
         </table>
-        <tbody>
-        </tbody>
         `;
-    }
-
-    update(): void {
-        this.elemento.innerHTML = this.template();
     }
 }
